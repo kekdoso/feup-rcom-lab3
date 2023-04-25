63285c4fe59fc609592a3b5e0c64708c29a447da
kekdoso <130851506+kekdoso@users.noreply.github.com>
2023-04-25 23:16:11 +0100

    Add files via upload

--- src/StateMachine.c
+++ src/StateMachine.c
@@ -1,39 +1,57 @@
-switch(currentState){
+typedef enum{
 
-    case Start:
+    stateStart,
+    stateFlagRCV,
+    stateARCV,
+    stateCRCV,
+    stateBCCOK,
+    stateStop,
+}
+
+switch(frameState){
+
+    case stateStart:
         if buf[0] == FLAG_RCV
-            currentState = stateFlagRCV;
+            frameState = stateFlagRCV;
         break;
 
     case stateFlagRCV:
         if buf[0] == FLAG_RCV
-            currentState = stateFlagRCV;
+            frameState = stateFlagRCV;
         if buf[0] == A_RCV
-            currentState = stateARCV;
+            frameState = stateARCV;
         if buf[0] == Other_RCV
-            currentState = stateOther;
+            frameState = stateOther;
         break;
     case stateARCV:
         if buf[0] == FLAG_RCV
-            currentState = stateFlagRCV;
+            frameState = stateFlagRCV;
         if buf[0] == C_RCV
+        {
             if (C_RCV == 1) 
-                currentState = stateFlagRCV;
+                frameState = stateFlagRCV;
+        }
         if buf[0] == Other_RCV
-            currentState = stateOther;             
+            frameState = stateOther;             
         break;  
 
     case stateCRCV:
         if buf[0] == Other_RCV
-            currentState = stateOther;
+            frameState = stateOther;
         if buf[0] == FLAG_RCV
-            currentState = stateFlagRCV;
+            frameState = stateFlagRCV;
         if (A^C == buf[0])
-            currentstate = BCCOK;
+            frameState = stateBCCOK;
         break;
     
-    case BCCOK:
+    case stateBCCOK:
         if buf[0] == FLAG_RCV
-            currentState = Stop;
+            frameState = stateStop;
+        if buf[0] == Other_RCV
+            frameState = stateStart;
+        break;
+    
+    case stateStop: 
+        break;
             
 } 
