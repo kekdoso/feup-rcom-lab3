1edf0b50d7571812810339f733d452c7f45c3c4e
carina505 <130772222+carina505@users.noreply.github.com>
2023-04-26 19:01:56 +0100

    Add files via upload

--- src/writenoncanonical.c
+++ src/writenoncanonical.c
@@ -14,11 +14,32 @@
 #define FALSE 0
 #define TRUE 1
 
+#define FLAG_RCV 0x5c
+#define A_RCV 0x03
+#define ALT_A_RCV 0x01
+#define C_SET 0x03
+#define C_UA 0x07
+
 volatile int STOP = FALSE;
+
+typedef enum{
+
+    stateStart,
+    stateFlagRCV,
+    stateARCV,
+    stateCRCV,
+    stateBCCOK,
+    stateOther,
+    stateStop,
+} frameStates;
+
+frameStates frameState = stateStart;
+
+int C_RCV = C_UA; /* in here, we receive the UA message */
 
 int main(int argc, char** argv)
 {
-    int fd, c, res, res2;
+    int fd, c, res, res2, flagged = 0;
     struct termios oldtio, newtio;
     char buf[255], str[255], str2[255];
     int i, sum = 0, speed = 0;
@@ -73,23 +94,116 @@ int main(int argc, char** argv)
 
     printf("New termios structure set\n");
     
-    printf("Enter a string : ");
-    gets(str);
-    strcpy(buf,str);
+    //------------------------------------
+    /*printf("Enter a string : ");
+    gets(str);*/
+
+    str[0] = 0x5c;      /*   F   */
+    str[1] = 0x03;      /*   A   */
+    str[2] = 0x03;      /*   C   */
+    str[3] = 0x03^0x03; /* BCCOK */
+    str[4] = 0x5c;      /*   F   */
+    
+    strcpy(buf,str);    
     
     /*testing*/
-    buf[strlen(str)] = '\0';
+    //buf[strlen(str)] = '\0';
 
-    res = write(fd,buf,strlen(str)+1);
+    res = write(fd,str,5);
     printf("%d bytes written\n", res);
 
-    while (STOP == FALSE) {       // loop for input 
-        res = read(fd,buf,255);   // returns after 5 chars have been input
-        buf[res] = 0;             // so we can printf...
+    /*while (STOP == FALSE) {           // loop for input 
+        res = read(fd,buf,255);       // returns after 5 chars have been input
+        buf[res] = 0;                 // so we can printf...
         printf(":%s:%d\n", buf, res);
         //printf("%d\n", strlen(buf));
         if (buf[0] == '\0') STOP = TRUE;
+    }*/
+
+    printf("UA State Machine has started\n"); 
+
+    while (STOP == FALSE) {       /* loop for input */
+        res = read(fd,buf,1);   /* returns after 5 (1) chars have been input */        
+        //buf[res]=0;               /* so we can printf... */
+        //printf(":%s:%d", buf, res);
+        printf("var=0x%02x\n",(unsigned int)(buf[0] & 0xff));
+        switch(frameState){
+
+            case stateStart:
+                if (buf[0] == FLAG_RCV){
+                    frameState = stateFlagRCV;
+                    printf("stateFlagRCV\n");
+                }
+                break;
+
+            case stateFlagRCV:
+                if (buf[0] == FLAG_RCV){
+                    frameState = stateFlagRCV;
+                    printf("stateFlagRCV\n");
+                }
+                else if (buf[0] == A_RCV || buf[0] == ALT_A_RCV){
+                    frameState = stateARCV;
+                    printf("stateARCV\n");
+                }
+                else{
+                    frameState = stateStart; 
+                    printf("stateStart\n");            
+                    flagged = 0;
+                }
+                break;
+            case stateARCV:
+                if (buf[0] == FLAG_RCV){
+                    frameState = stateFlagRCV;
+                    printf("stateFlagRCV\n");
+                }
+                else if (buf[0] == C_RCV){
+                    frameState = stateCRCV;
+                    printf("stateCRCV\n");
+                }
+                else{
+                    frameState = stateStart; 
+                    printf("stateStart\n");            
+                    flagged = 0;
+                }
+                break;  
+
+            case stateCRCV:
+                if (buf[0] == FLAG_RCV){
+                    frameState = stateFlagRCV;
+                    printf("stateFLAGRCV\n");
+                }
+                else if (buf[0] == A_RCV^C_RCV || buf[0] == ALT_A_RCV^C_RCV){
+                    frameState = stateBCCOK;
+                    printf("stateBCCOK\n");
+                }
+                else{
+                    frameState = stateStart;
+                    printf("stateStart\n");
+                    flagged = 0;
+                }
+                break;
+            
+            case stateBCCOK:
+                if (buf[0] == FLAG_RCV){
+                    //frameState = stateStop;
+                    printf("stateStop\n"); 
+                }
+                else{
+                    frameState = stateStart;
+                    printf("stateStart\n");
+                    flagged = 0;
+                }
+                break;
+                              
+        } 
+        
+        if (buf[0]==FLAG_RCV && flagged==1)
+            STOP=TRUE;
+        if(buf[0] == FLAG_RCV)
+            flagged = 1;
+        printf("%s:%d\n", buf, res);  
     }
+   
 
     sleep(1);
     if ( tcsetattr(fd,TCSANOW,&oldtio) == -1) {
