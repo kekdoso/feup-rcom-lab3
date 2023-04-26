95f66a23d5ceaaaf737e7c1397838e3cc270eb41
kekdoso <130851506+kekdoso@users.noreply.github.com>
2023-04-26 19:01:18 +0100

    Add files via upload

--- src/noncanonical.c
+++ src/noncanonical.c
@@ -12,11 +12,33 @@
 #define FALSE 0
 #define TRUE 1
 
+#define FLAG_RCV 0x5c
+#define A_RCV 0x03
+#define ALT_A_RCV 0x01
+#define C_SET 0x03
+#define C_UA 0x07
+
 volatile int STOP=FALSE;
+
+typedef enum{
+
+    stateStart,
+    stateFlagRCV,
+    stateARCV,
+    stateCRCV,
+    stateBCCOK,
+    stateOther,
+    stateStop,
+} frameStates;
+
+frameStates frameState = stateStart;
+
+int C_RCV = C_SET;
 
 int main(int argc, char** argv)
 {
     int fd,c, res, res2;
+    int flagged = 0, i = 0;
     struct termios oldtio,newtio;
     char buf[255], str[255];
 
@@ -68,16 +90,101 @@ int main(int argc, char** argv)
     }
 
     printf("New termios structure set\n");
+    printf("State Machine SET started\n"); 
 
     while (STOP == FALSE) {       /* loop for input */
         res = read(fd,buf,1);   /* returns after 5 (1) chars have been input */        
         //buf[res]=0;               /* so we can printf... */
         //printf(":%s:%d", buf, res);
-        strcat(str, buf);
-        if (buf[0]=='\0') STOP=TRUE;
+        printf("Var=0x%02x\n",(unsigned int)(buf[0] & 0xff));
+        switch(frameState){
+
+            case stateStart:
+                if (buf[0] == FLAG_RCV)
+                {
+                    frameState = stateFlagRCV;
+                    printf("stateFlagRCV\n");
+                }
+                break;
+
+            case stateFlagRCV:
+                if (buf[0] == FLAG_RCV){
+                    frameState = stateFlagRCV;
+                    printf("stateFlagRCV\n");
+                }
+                else if (buf[0] == A_RCV || buf[0] == ALT_A_RCV){
+                    frameState = stateARCV;
+                    printf("stateARCV\n");
+                }
+                else{
+                    frameState = stateStart; 
+                    printf("stateStart\n");            
+                    flagged = 0;
+                }
+                break;
+            case stateARCV:
+                if (buf[0] == FLAG_RCV){
+                    frameState = stateFlagRCV;
+                    printf("stateFlagRCV\n");
+                }
+                else if (buf[0] == C_RCV){
+                    frameState = stateCRCV;
+                    buf[0] = C_UA; //UA CHANGE
+                    printf("stateCRCV\n");
+                }
+                else{
+                    frameState = stateStart; 
+                    printf("stateStart\n");            
+                    flagged = 0;
+                }
+                break;  
+
+            case stateCRCV:
+
+                if (buf[0] == FLAG_RCV){
+                    frameState = stateFlagRCV;
+                    printf("stateFLAGRCV\n");
+                }
+                else if (buf[0] == A_RCV^C_RCV || buf[0] == ALT_A_RCV^C_RCV)
+                {
+                    frameState = stateBCCOK;
+                    printf("stateBCCOK\n");
+                }
+                else{
+                    frameState = stateStart;
+                    printf("stateStart\n");
+                    flagged = 0;
+                }
+                break;
+            
+            case stateBCCOK:
+                if (buf[0] == FLAG_RCV){
+                    //frameState = stateStop;
+                    printf("stateStop\n"); 
+                }
+                else{
+                    frameState = stateStart;
+                    printf("stateStart\n");
+                    flagged = 0;
+                }
+                break;
+            
+            /*case stateStop:
+                printf("stateStop\n"); 
+                break;*/
+                    
+        } 
+        str[i] = buf[0];
+        i++;
+        if (buf[0]==FLAG_RCV && flagged==1) 
+        {
+            STOP=TRUE;
+        }
+        if(buf[0]==FLAG_RCV)
+            flagged = 1;
         printf("%s:%d\n", buf, res);  
     }
-    res2 = write(fd,str,strlen(str)+1);
+    res2 = write(fd,str,i);
     //printf(":%s:%d\n", str, res2);  
     printf("%d bytes written\n", res2);
 
